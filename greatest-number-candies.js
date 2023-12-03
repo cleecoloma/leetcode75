9dcdb263fffc2d9b18d0d55b1810f5cbea0dd834
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-02 23:36:47 -0800

    added another code for greatest number of candies

--- greatest-number-candies.js
+++ greatest-number-candies.js
@@ -17,3 +17,18 @@ Big O Notation
 Time - O(2n) -> O(n)
 Space - O(n)
 */
+
+function kidsWithCandies(candies, extraCandies) {
+  const results = [];
+  const maxValue = Math.max(...candies);
+  for (let i = 0; i < candies.length; i++) {
+    results.push(candies[i] + extraCandies >= maxValue)
+  }
+  return results;
+};
+
+/* 
+Big O Notation
+Time - O(n)
+Space - O(n)
+*/
