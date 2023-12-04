803d455cc853f9117a1b1c4f6cebb971949d5cce
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-03 23:52:51 -0800

    added code for valid parenthesis

---
+++ valid-parenthesis.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function validParentheses(s) {
+  const stack = [];
+  const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+    if (map[c]) {
+      stack.push(map(c))
+    } else if (c !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return !stack.length;
+}
