434325c6e5159d61f5eb60a90c81da9c31856e79
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-06 16:39:43 -0800

    added binary search code

---
+++ binary-search.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function binarySearch(nums, target) {
+  let mid = Math.floor((nums.length - 1)/2);
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
