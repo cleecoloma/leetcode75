a5ee55da58926b5e57a7e8a30dada51580e221f4
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-07 23:24:26 -0800

    completed code for balance binary tree

--- balanced-binary-tree.js
+++ balanced-binary-tree.js
@@ -1,5 +1,20 @@
 'use strict';
 
 function isBalanced(root) {
-  
+  let balance = true;
+
+  function checker(node, height) {
+    if (!node) {
+      return 0;
+    }
+    let left = checker(node.left, height + 1)
+    let right = checker(node.right, height + 1);
+
+    if (Math.abs(left - right) > 1) {
+      balance = false;
+    }
+    return Math.max(left, right) + 1;
+  }
+  checker(root, 0);
+  return balance;
 }
