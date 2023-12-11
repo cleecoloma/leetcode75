139d48c274b30f5f39e4473f91e6a8a4cc3fd01d
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-10 22:39:29 -0800

    added code for linked list cycle

--- linked-list-cycle.js
+++ linked-list-cycle.js
@@ -1,2 +1,14 @@
 'use strict';
 
+function hasCycle(head) {
+  let visitedNodes = new Map();
+  let currentNode = head;
+  while (currentNode !== null) {
+    if (visitedNodes.has(currentNode)) {
+      return true;
+    }
+    visitedNodes.set(currentNode, currentNode.value);
+    currentNode = currentNode.next;
+  }
+  return false;
+}
