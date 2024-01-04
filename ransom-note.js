e6ab6fe59aaa278964c2e038ceed0acef6b0609a
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-03 21:10:29 -0800

    added solution

--- ransom-note.js
+++ ransom-note.js
@@ -1,7 +1,28 @@
 'use strict';
 
-function ransomNote(note, magazine) {
-  
+function ransomNote(ransomNote, magazine) {
+  const counter = {};
+
+  // map each magazine char to a value
+  for (let char of magazine) {
+    if (counter[char]) {
+      counter[char]++
+    } else {
+      counter[char] = 1;
+    }
+  }
+
+  // check ransomNote char is in counter 
+
+  for (let letter of ransomNote) {
+    if (!counter[letter] || counter[letter] === 0) {
+      return false;
+    } else {
+      counter[letter]--;
+    }
+  }
+
+  return true;
 }
 
 // Big O Notation
