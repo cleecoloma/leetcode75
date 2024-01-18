f956d604c4277db9c993e62557a6af6e967e3068
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-17 21:18:26 -0800

    added code

--- majority-element.js
+++ majority-element.js
@@ -1,7 +1,17 @@
 'use strict';
 
 function majorityElement(nums) {
+  let element = null;
+  let count = 0;
 
+  for (const num of nums) {
+    if (count === 0) {
+      element = num;
+    }
+    count += (num === element) ? 1 : -1;
+  }
+
+  return element;
 }
 
 // Big O Notation
