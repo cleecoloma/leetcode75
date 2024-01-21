5d568b1c1a40a8bdfeefbe6870955b48ed5d1b35
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-20 22:15:49 -0800

    added solution to find if array has duplicate

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function containsDuplicate(nums) {
-  \
+  const mapper = {};
+  for (let num of nums) {
+    if (mapper[num]) {
+      return true;
+    } else {
+      mapper[num] = 1;
+    }
+  }
+  return false;
 }
 
 // Big O Notation
-// Time - 
-// Space - 
+// Time -
+// Space -
