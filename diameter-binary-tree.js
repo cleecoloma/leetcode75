9201ce196b0a00aa9c5229341d165491aba57b90
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-23 21:33:08 -0800

    added solution

--- diameter-binary-tree.js
+++ diameter-binary-tree.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function diameterOfBinaryTree(root) {
+  let diameter = 0;
 
+  function dfs(node) {
+    if (!node) {
+      return 0;
+    }
+
+    const left = dfs(node.left);
+    const right = dfs(node.right);
+
+    diameter = Math.max(diameter, left + right)
+    return Math.max(left, right) + 1
+  }
+
+  dfs(root);
+  return diameter;
 }
 
 // Big O Notation
