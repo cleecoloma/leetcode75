f740f54963419e7617f255f531775f8661a8036f
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-06 21:09:42 -0800

    added solution

--- insert-interval.js
+++ insert-interval.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function insertInterval(intervals, newInterval) {
-  
+  let [start, end] = newInterval;
+  let left = [];
+  let right = [];
+
+  for (const interval of intervals) {
+    const [first, last] = interval;
+    if (last < start) left.push(interval);
+    else if (first > end) right.push(interval);
+    else {
+      start = Math.min(start, first);
+      end = Math.max(end, last);
+    }
+  }
+
+  return [...left, [start, end], ...right];
 }
 
 // Big O Notation
