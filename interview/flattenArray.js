95f102243b132730c5dbfb38024bbe33f9368a30
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-08 21:34:26 -0800

    added solution to flatten array

--- interview/flattenArray.js
+++ interview/flattenArray.js
@@ -1,9 +1,23 @@
 'use strict';
 
 function flattenArray(nestedArray) {
-  
+  let results = [];
+
+  function flatten(element) {
+    if (Array.isArray(element)) {
+      for (let i = 0; i < element.length; i++) {
+        flatten(element[i]);
+      }
+    } else {
+      results.push(element);
+    }
+  }
+
+  flatten(nestedArray);
+
+  return results;
 }
 
 // Big O Notation
-// Time - 
-// Space - 
+// Time -
+// Space -
