20ba8a1eac4ced691397a70514ebfa3f5be926b5
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-12 12:19:56 -0700

    updated solution and added big O notation

--- reverse-words.js
+++ reverse-words.js
@@ -1,9 +1,27 @@
 'use strict';
 
-function reverseString(s) {
-  
+function reverseWords(s) {
+  function reverseStrings(word) {
+    let chars = word.split('')
+    let start = 0;
+    let end = chars.length - 1;
+    while (start < end) {
+      let temp = chars[start];
+      chars[start] = chars[end];
+      chars[end] = temp;
+      start++;
+      end--;
+    }
+    return chars.join('');
+  }
+
+  let words = s.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = reverseStrings(words[i]);
+  }
+  return words.join(" ");
 }
 
 // Big O Notation
 // Time - O(n)
-// Space - O(1)
+// Space - O(n)
