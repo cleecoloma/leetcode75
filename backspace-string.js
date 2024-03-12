7f1ce596a929e388c44a9bbc1d0b2d5df9f7b862
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-11 22:15:30 -0700

    added solution using two pointers

--- backspace-string.js
+++ backspace-string.js
@@ -1,7 +1,34 @@
 'use strict';
 
 function backspaceCompare(s, t) {
-  
+  let sChars = s.split('');
+  let tChars = t.split('');
+
+  let k = processString(sChars);
+  let p = processString(tChars);
+
+  if (k !== p) {
+    return false;
+  }
+
+  for (let i = 0; i < k; i++) {
+    if (sChars[i] !== tChars[i]) {
+      return false;
+    }
+  }
+  return true
+
+  function processString(chars) {
+    let k = 0;
+    for (let c of chars) {
+      if (c !== '#') {
+        chars[k++] = c;
+      } else if (k > 0) {
+        k--;
+      }
+    }
+    return k;
+  }
 }
 
 // Big O Notation
