da5f0b1769ed7aa6f203b449e13344deeb658bb6
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-12 11:34:13 -0700

    added solution to reverse string

--- reverse-string.js
+++ reverse-string.js
@@ -1,6 +1,20 @@
 'use strict';
 
-function reverseString(s) {}
+function reverseString(s) { // word
+  let start = 0;
+  let end = s.length - 1;
+  let temp = '';
+
+  while (start < end) {
+    temp = s[start]; // w
+    s[start] = s[end]; // d
+    s[end] = temp;
+    start++;
+    end--;
+  }
+
+  return s;
+}
 
 // Big O Notation
 // Time -
