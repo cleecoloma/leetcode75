663f91ee868b702390aff62cdd87c061902a8179
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-11 22:42:56 -0700

    added solution using map and loops

--- single-number.js
+++ single-number.js
@@ -1,9 +1,19 @@
 'use strict';
 
 function singleNumber(nums) {
- 
+  const map = {};
+  for (let n of nums) {
+    if (map[n] == null) {
+      map[n] = 0;
+    }
+    map[n]++;
+  }
+
+  for (let n in map) {
+    if (map[n] === 1) return Number(n);
+  }
 }
 
 // Big O Notation
-// Time - 
-// Space - 
+// Time - O(n)
+// Space - O(n)
