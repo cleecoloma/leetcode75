f3d0cba122306b4c9744dca52f8d4feffad9c25c
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-13 21:42:56 -0700

    added time and space complexity

--- max-substring.js
+++ max-substring.js
@@ -1,9 +1,24 @@
 'use strict';
 
 function lengthOfLongestSubstring(s) {
-  
+  let start = 0;
+  let maxLength = 0;
+  const charIndexMap = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (charIndexMap.has(char)) {
+      start = Math.max(charIndexMap.get(char) + 1, start);
+    }
+
+    charIndexMap.set(char, i);
+    maxLength = Math.max(maxLength, i - start + 1);
+  }
+
+  return maxLength;
 }
 
 // Big O Notation
-// Time - 
-// Space - 
+// Time - O(n)
+// Space - O(min(m,n)) where m is the # of characters in the map and n is the length of the input string;
