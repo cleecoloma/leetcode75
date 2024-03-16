64df558beba3a0aa4e50f6d00bf31a040c54ad5f
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-15 21:33:58 -0700

    added solution to code

--- move-zeros.js
+++ move-zeros.js
@@ -1,9 +1,19 @@
 'use strict';
 
 function moveZeroes(nums) {
-  
+  let index = 0; 
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[index] = nums[i];
+      index++;
+    }
+  }
+  for (let i = index; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
 }
 
 // Big O Notation
-// Time - 
-// Space - 
+// Time -
+// Space -
