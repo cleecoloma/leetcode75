c548b38707c115e75116f656be45ac569c884d4b
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-18 17:31:47 -0700

    completed solution

--- missing-number.js
+++ missing-number.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function missingNumber(nums) {
-  
+  const n = nums.length;
+  const expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    actualSum = actualSum + nums[i];
+  }
+
+  return expectedSum-actualSum;
 }
 
 // Big O Notation
-// Time - 
-// Space - 
+// Time -
+// Space -
