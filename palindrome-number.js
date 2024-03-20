f580a13392f183ebcf443f75171ffa30124c75b5
Chester Lee Coloma <cleecoloma@gmail.com>
2024-03-19 21:09:29 -0700

    added solution but doesnt pass all test cases

--- palindrome-number.js
+++ palindrome-number.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function isPalindrome(x) {
-  
+  if (x[0] === '-') {
+    return false;
+  }
+
+  let start = 0;
+  let end = x.length - 1;
+
+  while (start < end) {
+    if (x[start] !== x[end]) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+
+  return true;
 }
 
 // Big O Notation
