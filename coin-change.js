1642912f7c732f237ce4aa154ff6e5814230fb34
Chester Lee Coloma <cleecoloma@gmail.com>
2024-04-10 19:58:28 -0700

    added solution

--- coin-change.js
+++ coin-change.js
@@ -1,8 +1,20 @@
 'use strict';
 
 function coinChange(coins, amount) {
-  
+  const dp = new Array(amount + 1). fill(amount + 1);
+  dp[0] = 0;
+
+  for (let amt = 1; amt <= amount; amt++) {
+    for (let coin of coins) {
+      if (coin <= amt) {
+        dp[amt] = Math.min(dp[amt], 1 + dp[amt - coin]);
+      }
+    }
+  }
+
+  return dp[amount] > amount ? -1 : dp[amount];
 }
+
 // Big O Notation
 // Time - 
 // Space - 
