273e62e5fda5f3215c91f7e4f57e867c8fe677f8
Chester Lee Coloma <cleecoloma@gmail.com>
2024-04-11 23:34:18 -0700

    added recursive solution

--- same-tree.js
+++ same-tree.js
@@ -4,6 +4,14 @@ function sameTree(p, q) {
   return JSON.stringify(p) === JSON.stringify(q);
 }
 
+function sameTreeV2(p, q) {
+  // the same if both nodes are null
+  if (!p && !q) return true;
+  // not the same if either of the nodes is null or the values are different
+  if ((!p && q) || (p && !q) || p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
+
 // Big O Notation
 // Time -
 // Space -
