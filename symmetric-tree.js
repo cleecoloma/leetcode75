834b2ce46f120ef9a227da7a4e4a5e8f4733dcf5
Chester Lee Coloma <cleecoloma@gmail.com>
2024-04-12 23:34:11 -0700

    added solution using recursive

--- symmetric-tree.js
+++ symmetric-tree.js
@@ -1,6 +1,25 @@
 'use strict';
 
-function isSymmetric(root) {}
+function isSymmetric(root) {
+  if (root == null) {
+    return true;
+  }
+
+  return symmetricChecker(root.left, root.right);
+
+  function symmetricChecker(left, right) {
+    if (left == null && right == null) {
+      return true;
+    }
+    if (left == null || right == null) {
+      return false;
+    }
+    if (left.val !== right.val) {
+      return false;
+    }
+    return symmetricChecker(left.left, right.right) && symmetricChecker(left.right, right.left);
+  }
+}
 
 // Big O Notation
 // Time -
